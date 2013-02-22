826a0650c4e5cb5262ca4f4f1b4f7e351b866ec7
Ahmed Abdalla <ahmad19526@gmail.com>
2013-02-21 21:18:56 -0500

    added south reversion to settings.base

--- html5games/settings/base.py
+++ html5games/settings/base.py
@@ -131,6 +131,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'south',
+    'reversion',
 )
 
 # A sample logging configuration. The only tangible logging
